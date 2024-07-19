7cb8f9177295ec092c6613cbc4a6656705171420
Asim Basak <basak.asim.cs@gmailcom>
2024-07-19 16:51:53 +0530

    modified:   setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="krish",
+    version="0.1",
+    packages=find_packages(),
+    description="A simple Python package for basic math operations",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/asimbasak/krish",
+    license="MIT",
+)
