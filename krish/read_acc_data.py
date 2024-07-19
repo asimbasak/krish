f04d670c8432f87427dbabd8c5b9249e2c9fccff
Asim Basak <basak.asim.cs@gmailcom>
2024-07-19 17:05:56 +0530

    Changes to be committed: 	modified:   krish/read_acc_data.py

--- krish/read_acc_data.py
+++ krish/read_acc_data.py
@@ -3,6 +3,9 @@
 #===============================================================
 import pandas as pd
 
+def read_csv_file(file_name):
+    return pd.read_csv(file_name)
+
 def get_time_acc(filename,TimeCol, AccCol):
     df = pd.read_csv(filename)
 
