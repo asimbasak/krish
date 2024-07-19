88b6dcb89bce8f676fc437ce87662d61c8572b71
Asim Basak <basak.asim.cs@gmailcom>
2024-07-19 16:49:06 +0530

    modified:   LICENSE 	new file:   krish/__init__.py 	new file:   krish/math_operations.py 	new file:   setup.py

---
+++ krish/__init__.py
@@ -0,0 +1,3 @@
+from .math_operations import add_two_numbers
+
+__all__ = ["add_two_numbers"]
---
+++ krish/math_operations.py
@@ -0,0 +1,12 @@
+def add_two_numbers(a, b):
+    """
+    Adds two numbers and returns the result.
+
+    Parameters:
+    a (int or float): The first number.
+    b (int or float): The second number.
+
+    Returns:
+    int or float: The sum of the two numbers.
+    """
+    return a + b
